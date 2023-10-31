c598c772bd2861ef141ea79d5b0db75d06a302c1
NomkhosiDube <nomkhosidube2@gmail.com>
2023-10-31 04:02:50 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
-include "main.h"
+#include "main.h"
+
 
 
 /**
@@ -9,37 +10,30 @@ file and should prints it to the POSIX standard output
 Return: actual number of letters or 0 if falls
 **/
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL) {
-        return 0;
-    }
-    
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        return 0;
-    }
-    
-    char *buffer = (char *) malloc(sizeof(char) * (letters + 1));
-    if (buffer == NULL) {
-        close(fd);
-        return 0;
-    }
-    
-    ssize_t bytes_read = read(fd, buffer, letters);
-    if (bytes_read == -1) {
-        close(fd);
-        free(buffer);
-        return 0;
-    }
-    
-    ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read) {
-        close(fd);
-        free(buffer);
-        return 0;
-    }
-    
-    close(fd);
-    free(buffer);
-    return bytes_read;
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
